6fdef089a6521fb720ac12e8d5c484e0b7ce6804
7usien <husienadel2022@gmail.com>
2022-12-31 00:40:17 +0300

    add caching with redux persist

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
-import { store } from './redux/store';
-
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={'loading'} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
